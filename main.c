086e966c43c02d251b96c48006ba9b156bde1985
sudo0272 <sudo180272@protonmail.ch>
2020-08-27 01:56:19 +0900

    Fix bug
    Bug;
    flushing bug
    -> solved with getchar()
    generated file don't have tail of main function and return
    -> add them
    \n must be written to file but NL is written
    -> change \n to \\n

--- main.c
+++ main.c
@@ -16,17 +16,23 @@ int main() {
   int j;
 
   printf("Enter the range of numbers to be able to be calculated by splitting "
-         "them with space");
+         "them with space\n");
   printf("> ");
   scanf("%d %d", &range.start, &range.end);
+
+  getchar(); // flush
 
   printf("Enter the file which the codes be written\n");
   printf("> ");
   scanf("%s", codePath);
 
+  getchar();// flush
+
   printf("Enable short-code mode? (Y/n)\n");
   printf("> ");
   scanf("%c", &shortCodeMode);
+
+  getchar(); // flush
 
   switch (shortCodeMode) {
     case 'Y':
@@ -50,6 +56,7 @@ int main() {
     fprintf(codeFile, "scanf(\"%%d %%d\",&n,&m);");
   } else {
     fprintf(codeFile, "#include <stdio.h>\n");
+    fprintf(codeFile, "\n");
     fprintf(codeFile, "int main() {\n");
     fprintf(codeFile, "  int n;\n");
     fprintf(codeFile, "  int m;\n");
@@ -62,18 +69,26 @@ int main() {
   if (isShortCodeModeEnabled) {
     for (i = range.start; i <= range.end; i++) {
       for (j = range.start; j <= range.end; j++) {
-        fprintf(codeFile, "if(n==%d&&m==%d)printf(\"%d\");", i, j, i + j);
+        fprintf(codeFile, "if(n==%d&&m==%d)printf(\"%d\\n\");", i, j, i + j);
       }
     }
   } else {
     for (i = range.start; i <= range.end; i++) {
       for (j = range.start; j <= range.end; j++) {
         fprintf(codeFile, "  if (n == %d && m == %d) {\n", i, j);
-        fprintf(codeFile, "    printf(\"%d\n\")", i + j);
+        fprintf(codeFile, "    printf(\"%d\\n\");\n", i + j);
         fprintf(codeFile, "  }\n");
         fprintf(codeFile, "\n");
       }
     }
+  }
+
+  if (isShortCodeModeEnabled) {
+    fprintf(codeFile, "return 0;}");
+  } else {
+    fprintf(codeFile, "  return 0;\n");
+    fprintf(codeFile, "}\n");
+    fprintf(codeFile, "\n");
   }
 
   return 0;
