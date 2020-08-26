e642853fcf57c68537d5cb5ae7b86d9768295ccb
sudo0272 <sudo180272@protonmail.ch>
2020-08-27 01:41:59 +0900

    Implement getting information and writing basic code and addition code
    Information; range of numbers, file path and short code mode
    Basic code; include, main function and declaring variables

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+  int start;
+  int end;
+} Range;
+
+int main() {
+  Range range;
+  char shortCodeMode;
+  bool isShortCodeModeEnabled;
+  char codePath[256];
+  FILE *codeFile;
+  int i;
+  int j;
+
+  printf("Enter the range of numbers to be able to be calculated by splitting "
+         "them with space");
+  printf("> ");
+  scanf("%d %d", &range.start, &range.end);
+
+  printf("Enter the file which the codes be written\n");
+  printf("> ");
+  scanf("%s", codePath);
+
+  printf("Enable short-code mode? (Y/n)\n");
+  printf("> ");
+  scanf("%c", &shortCodeMode);
+
+  switch (shortCodeMode) {
+    case 'Y':
+    case 'y':
+    case '\n':
+      isShortCodeModeEnabled = true;
+
+      break;
+
+    default: // includes N and n
+      isShortCodeModeEnabled  = false;
+
+      break;
+  }
+  
+  codeFile = fopen(codePath, "w");
+  
+  if (isShortCodeModeEnabled) {
+    fprintf(codeFile, "n,m;");
+    fprintf(codeFile, "main(){");
+    fprintf(codeFile, "scanf(\"%%d %%d\",&n,&m);");
+  } else {
+    fprintf(codeFile, "#include <stdio.h>\n");
+    fprintf(codeFile, "int main() {\n");
+    fprintf(codeFile, "  int n;\n");
+    fprintf(codeFile, "  int m;\n");
+    fprintf(codeFile, "\n");
+    fprintf(codeFile, "  scanf(\"%%d %%d\", &n, &m);\n");
+    fprintf(codeFile, "\n");
+  }
+
+  // Addition
+  if (isShortCodeModeEnabled) {
+    for (i = range.start; i <= range.end; i++) {
+      for (j = range.start; j <= range.end; j++) {
+        fprintf(codeFile, "if(n==%d&&m==%d)printf(\"%d\");", i, j, i + j);
+      }
+    }
+  } else {
+    for (i = range.start; i <= range.end; i++) {
+      for (j = range.start; j <= range.end; j++) {
+        fprintf(codeFile, "  if (n == %d && m == %d) {\n", i, j);
+        fprintf(codeFile, "    printf(\"%d\n\")", i + j);
+        fprintf(codeFile, "  }\n");
+        fprintf(codeFile, "\n");
+      }
+    }
+  }
+
+  return 0;
+}
+
